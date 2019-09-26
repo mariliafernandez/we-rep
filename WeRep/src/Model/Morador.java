f2d22e9ca221932cb437d03716e89c489e6b643d
alvaropq <36276084+alvaropq@users.noreply.github.com>
2019-09-26 00:28:45 -0300

    Update Morador.java

--- WeRep/src/Model/Morador.java
+++ WeRep/src/Model/Morador.java
@@ -3,12 +3,126 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package model;
+
+import java.util.Objects;
 
 /**
  *
- * @author Álvaro
+ * @author Álvaro Queiroz
  */
 public class Morador {
+    private Double saldo;
+    private String nome;
+    private String sobreNome;
+    private String email;
+    private String login;
+    private String senha;
+    
+    public Morador(Double saldo, String nome, String sobreNome) {
+        this.saldo = saldo;
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+    }
+    public Morador() {
+    }
+    
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    @Override
+    public String toString() {
+        return "Morador{" + "saldo=" + saldo + ", nome=" + nome + ", sobreNome=" + sobreNome + ", email=" + email + ", login=" + login + ", senha=" + senha + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.saldo);
+        hash = 37 * hash + Objects.hashCode(this.nome);
+        hash = 37 * hash + Objects.hashCode(this.sobreNome);
+        hash = 37 * hash + Objects.hashCode(this.email);
+        hash = 37 * hash + Objects.hashCode(this.login);
+        hash = 37 * hash + Objects.hashCode(this.senha);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Morador other = (Morador) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.sobreNome, other.sobreNome)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        if (!Objects.equals(this.senha, other.senha)) {
+            return false;
+        }
+        if (!Objects.equals(this.saldo, other.saldo)) {
+            return false;
+        }
+        return true;
+    }
     
 }
